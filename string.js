9b4bf6b8b03c9e831c63a396b112f3410cad0df0
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2019-07-25 15:18:00 -0400

    Update string.js

--- string.js
+++ string.js
@@ -1,10 +1,14 @@
-const snakeCase = (text)=>text.toLowerCase().replace(/[^a-zA-Z0-9\s]/g, '').replace(/\s/g, '_'),
-const titleCase = (text)=>text.replace(/\w\S*/g, (txt)=>txt.charAt(0).toUpperCase()+txt.substr(1).toLowerCase()),
-const kebobCase = (text)=>text.toLowerCase().replace(/[^\w]+/g, '-'),
-const camelCase = (text)=>text.replace(/^([A-Z])|[\s-_]+(\w)/g, (_, a, b)=>b?b.toUpperCase():a.toLowerCase()),
+const snakeCase = (text)=>text.toLowerCase().replace(/[^a-zA-Z0-9\s]/g, '').replace(/\s/g, '_');
+const titleCase = (text)=>text.replace(/\w\S*/g, (txt)=>txt.charAt(0).toUpperCase()+txt.substr(1).toLowerCase());
+const kebobCase = (text)=>text.toLowerCase().replace(/[^\w]+/g, '-');
+const camelCase = (text)=>text.replace(/^([A-Z])|[\s-_]+(\w)/g, (_, a, b)=>b?b.toUpperCase():a.toLowerCase());
 
+// Get all regex matches
+const execAll = (rgx, str)=>{
+	let matches, res = [];
+	while (matches = rgx.exec(str)){res.push(matches[1]);};
+	return res;
+};
+      
+      
 //format money?
-
-
-module.exports = {snakeCase,titleCase,kebobCase,camelCase};
-
