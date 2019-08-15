c165d699b54e5faa12889a8e939fd5e16444e64d
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2019-08-15 10:14:22 -0400

    Update vitreum.js

--- vitreum.js
+++ vitreum.js
@@ -5,35 +5,38 @@
 
 const ReactDOMServer = require('react-dom/server'), React = require('react'), browserify = require('browserify');
 
-let Libs, LibsCache;
-const pack = async (bundler)=>new Promise((res, rej)=>bundler.bundle((err, buf)=>err ? rej(err) : res(buf.toString())));
-const getHTML = (bundle, props)=>`<main id='root'>${ReactDOMServer.renderToString(React.createElement(eval(`module=undefined;${bundle};global.Root`), props))}</main>`;
-const getHydrate = (props)=>`require('react-dom').hydrate(require('react').createElement(Root, ${JSON.stringify(props)}),document.getElementById('root'));`;
-const getLibs = async ()=>{if(!LibsCache){ LibsCache = await pack(browserify().require(Libs).transform('uglifyify', { global : true }));}; return LibsCache;};
-const postFilter = (id, filepath)=>{
-	if(!filepath) throw `Can not find module '${id}'`;
-	return (filepath.indexOf('node_modules') === -1) ? true : (Libs.push(id) && false);
-}
-const getBundler = (entryPath, opts = {})=>{
-	Libs = ['react', 'react-dom'];
-	return browserify({ standalone : 'Root', postFilter, ...opts })
-		.require(entryPath).transform('babelify', { presets : ['@babel/preset-react'] });
-};
-const collect = async (bundler, props)=>{
-	const bundle = await pack(bundler);
-	return {
-		bundle : `${bundle};${getHydrate(props)}`,
-		libs   : await getLibs(),
-		html   : getHTML(bundle, props),
-	};
-};
+let LibsList;
+const pack       = async (bundler)=>new Promise((res, rej)=>bundler.bundle((err, buf)=>err ? rej(err) : res(buf.toString())));
+const getHTML    = (bundle, props={})=>`<main id='root'>${ReactDOMServer.renderToString(React.createElement(eval(`module=undefined;${bundle};global.Root`), props))}</main>`;
+const getHydrate = (props, ssr=false)=>`require('react-dom').${ssr ? 'hydrate':'render'}(require('react').createElement(Root, ${JSON.stringify(props)}),document.getElementById('root'));`;
+const getLibs    = async (libs)=>await pack(browserify().require(libs).transform('uglifyify', { global : true }));
+const getBundler = (entryPoint, opts = {})=>browserify({ standalone : 'Root', postFilter, cache : {}, ...opts }).require(entryPoint).transform('babelify', { presets : ['@babel/preset-react'] });
+const postFilter = (id, path)=>{if(!path){throw `Can not find module '${id}'`;} return (path.indexOf('node_modules') === -1) ? true : (LibsList.push(id) && false); };
 
-const build = async (entryPath, props, opts = {})=>await collect(getBundler(entryPath, opts), props);
-const dev = (entryPath, props, task = ({ bundle, html, libs })=>{}, opts={})=>{
-	const run = async ()=>task(await collect(bundler, props));
-	const bundler = getBundler(entryPath, { debug : true, cache : {}, ...opts }).plugin('watchify').on('update', run);
-	run();
+const defaultOpts = {
+	libs  : true,
+	ssr   : true,
+	dev   : false, //pass a function to run dev tasks
+	props : {}
 };
-const render = ({ head, html, bundle, libs })=>`<html><head>${head}</head><body>${html}</body><script>${libs}</script><script>${bundle}</script></html>`;
+const build = async (entryPoint, opts={})=>{
+	opts = {...defaultOpts, ...opts};
+	let bundler = getBundler(entryPoint, { debug : !!opts.dev })
+	const execute = async ()=>{
+		LibsList = ['react', 'react-dom']
+		let bundle, libs, html=`<main id='root'></main>`;
+		bundle = await pack(bundler);
+		if(opts.libs) libs = await getLibs(LibsList);
+		if(opts.ssr) html = getHTML(bundle, opts.props);
+		bundle += getHydrate(opts.props, opts.ssr);
+		return {bundle, libs, html};
+	}
+	if(opts.dev){
+		bundler = bundler.plugin('watchify').on('update', async ()=>opts.dev(await execute()));
+		return opts.dev(await execute());
+	}
+	return await execute();
+};
+const render = ({ head='', html, bundle, libs })=>`<html><head>${head}</head><body>${html}</body><script>${libs}</script><script>${bundle}</script></html>`;
 
-module.exports = {dev, build, render};
+module.exports = { build, render};
