412e819f76e6764f8274e1af6de4ccbb832cbad2
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2019-11-08 16:32:31 -0500

    Update async.js

--- async.js
+++ async.js
@@ -2,4 +2,13 @@ const wait = async (n,val)=>new Promise((r)=>setTimeout(()=>r(val), n));
 const loop = (fn)=>setTimeout(()=>loop(fn), fn() || 1000);
 const sequence = async (obj, fn)=>Object.keys(obj).reduce((a,key)=>a.then((r)=>fn(obj[key], key, r)), Promise.resolve());
 
+let queue;
+const enqueue = (func)=>{
+	return new Promise((resolve, reject)=>{
+		queue = (queue || Promise.resolve()).then(()=>{
+			return func().then(resolve).catch(reject);
+		}).finally(()=>{delete queue});
+	})
+};
+
 module.exports = {wait,loop,sequence};
