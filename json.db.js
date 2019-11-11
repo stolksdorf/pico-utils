484bc502890799344294d133d24cab5da39c91de
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2019-11-11 18:17:44 -0500

    Improvements to json.db.js

--- json.db.js
+++ json.db.js
@@ -1,29 +1,31 @@
 const fs = require('fs');
 
-module.exports = (filepath)=>{
-	let data = [];
-	const load = async ()=>data=JSON.parse((await fs.promises.readFile(filepath, 'utf8')));
+module.exports = (filepath, shouldWatch=true)=>{
+	let data = JSON.parse(fs.readFileSync(filepath, 'utf8'));
 	const save = async (val)=>{
 		data = val;
-		return fs.promises.writeFile(filepath, JSON.stringify(data, null, '\t')).then(()=>val);
+		return fs.promises.writeFile(filepath, JSON.stringify(data, null, '\t'));
 	}
-	fs.watch(filepath, load);
-	load();
+	if(shouldWatch) fs.watch(filepath, async ()=>{
+		//can be made sync
+		try{ data = JSON.parse((await fs.promises.readFile(filepath, 'utf8'))); }catch(err){}
+	});
 
 	const db = {
-		query : async (fn)=>data.filter(fn),
-		all   : async ()=>data,
-		get   : async (id)=>data.find((x)=>x.id==id),
+		query  : async (fn)=>data.filter(fn),
+		all    : async ()=>data,
+		get    : async (id)=>data.find((x)=>x.id==id),
 
+		set    : async (x)=>save(x),
 		clear  : async ()=>save([]),
-		add    : async (obj)=>save(data.concat(obj)),
+		add    : async (obj)=>save(data.concat(obj)).then(()=>obj),
 		remove : async (id)=>save(data.filter((x)=>x.id!=id)),
 		update : async (id, obj)=>{
 			const idx = data.findIndex((x)=>x.id==id);
-			if(idx==-1) return db.add(obj)
+			if(idx==-1) return db.add({id, ...obj});
 			data[idx] = {...data[idx], ...obj};
-			return save(data);
+			return save(data).then(()=>data[idx]);
 		}
 	}
 	return db;
-}
+};
