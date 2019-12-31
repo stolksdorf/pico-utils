42863e6dd01626334eb270be235de347a54be957
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2019-12-31 12:51:45 -0500

    Update other.js

--- other.js
+++ other.js
@@ -1,3 +1,16 @@
+const getArgs = (processArr = process.argv.slice(2))=>{
+	return processArr.reduce((acc, arg)=>{
+		if(arg[0]=='-'){
+			let [key,val] = arg.replace(/-(-)?/, '').split('=');
+			acc[key] = typeof val == 'undefined' ? true : val;
+			return acc;
+		}
+		acc.args.push(arg);
+		return acc;
+	}, {args:[]});
+};
+
+
 
 //set && get
 
