0d5185caf46af94488efd0ff06f01181f6606d46
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2020-01-26 16:52:17 -0500

    Update async.js

--- async.js
+++ async.js
@@ -1,7 +1,7 @@
 const wait = async (n,val)=>new Promise((r)=>setTimeout(()=>r(val), n));
 const loop = async (fn)=>setTimeout(()=>loop(fn), await fn() || 1000);
 const sequence = async (obj, fn)=>Object.keys(obj).reduce((a,key)=>a.then((r)=>fn(obj[key], key, r)), Promise.resolve());
-const debounce = function(fn, t=16){ clearTimeout(this.clk); this.clk = setTimeout(fn, t); };
+const debounce = (fn, t=16)=>function(...args){clearTimeout(this.clk);this.clk=setTimeout(()=>fn(...args),t);};
 
 const enqueue = function(fn){
 	return new Promise((rsv, rej)=>{
