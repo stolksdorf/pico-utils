513e744d04c7ccd23701572a2efabea8d0d19c15
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2020-02-06 10:47:33 -0500

    Update other.js

--- other.js
+++ other.js
@@ -53,25 +53,12 @@ const cookies = document.cookie.split(';').reduce((res, c) => {
 }, {});
 
 const _internalPaths = Object.keys(process.binding('natives')).concat(['bootstrap_node', 'node']).map((name) => `${name}.js`);
-const getStack = (err)=>{
-	return err.stack.split('\n').reduce((stack, raw)=>{
+const getStack = (idx=false)=>{
+	const stack = (new Error()).stack.split('\n').map((raw)=>{
 		const [_, name, file, line, col] =
 			/    at (.*?) \((.*?):(\d*):(\d*)\)/.exec(raw) || /    at ()(.*?):(\d*):(\d*)/.exec(raw) || [];
-		if(!!file && !_internalPaths.includes(file)){
-			stack.push({name, file:file.replace(process.cwd(), '').slice(1), line:Number(line), col:Number(col), raw})
-		}
-		return stack;
-	}, []);
-};
-
-const getTrace = (offset=0)=>{
-	const stack = (new Error()).stack.split('\n').reduce((stack, raw)=>{
-		const [_, name, file, line, col] =
-			/    at (.*?) \((.*?):(\d*):(\d*)\)/.exec(raw) || /    at ()(.*?):(\d*):(\d*)/.exec(raw) || [];
-		if(file) stack.push({
-			name, file : file.replace(process.cwd(), '').slice(1), line : Number(line), col  : Number(col), raw
-		});
-		return stack;
-	}, []).slice(offset);
-	return stack[0] || {};
+		return { name, file, line : Number(line), col  : Number(col), raw };
+	}).filter(({file})=>!!file && !_internalPaths.includes(file)).slice(1);
+	if(idx !== false) return stack[idx];
+	return stack;
 };
