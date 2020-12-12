94aee1f03dd3f9062bb07894c069d24c4594b8df
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2020-12-12 16:54:05 -0500

    Update parse_engine.js

--- parse_engine.js
+++ parse_engine.js
@@ -1,92 +1,31 @@
-/**
+/* Usage
 
-const input = `daaabc`
 
-const rules ={
-	aMatch : [/a(a)/, (match, [group1], ctx)=>'x'],
-	bMatch : [/b/, (match)=>'y'],
-	dMatch : [/d/, (match)=>'yo yo ya'],
-	default : (match)=>'ha'
+const rules = {
+	'aa' : ()=>'woo',
+	'bb(.)b' : ([letter], raw, pre)=>'foo' + letter + ` (${pre})`
 };
 
-console.log(engine(rules, input));
 
-**/
-const map = (obj,fn)=>Object.keys(obj).map((key)=>fn(obj[key],key));
+console.log(parse(rules, `g bbtb aa aa   goo bbdb`));
 
-
-const engine = (rules, input)=>{
-
-	let Rules = {}, defaultRule;
+*/
 
 
-	map(rules, (val, id)=>{
-		if(id == 'default'){
-			defaultRule = val;
-			return;
-		}
-		Rules[id] = {regex: val[0], render:val[1]}
-		if(Array.isArray(val[0])){
-			Rules[id].regex = val[0][0]
-			Rules[id].nest = val[0][1]
-		}
-	});
-
-	console.log(Rules);
-
-	const getBestMatch = (rules, text)=>{
-		const exec = (regex, text, id)=>{
-			const match = regex.exec(text);
-			if(!match) return false;
-			const [matchedText, ...groups] = match;
-			return {
-				id,
-				start : match.index,
-				end   : match.index + matchedText.length,
-				text  : matchedText,
-				input : text,
-				groups
-			};
-		}
-		let best;
-		Object.keys(rules).map((id)=>{
-			const match = exec(rules[id].regex, text, id);
-			if(match && (!best || match.start < best.start)){
-				best = match;
-			}
-		});
-		return best;
+const parse = (rules, text)=>{
+	const Rules = Object.entries(rules).map(([rgx, fn])=>[new RegExp(rgx),fn]);
+	let remaining = text, result = [];
+	while(remaining.length > 0){
+		let match = Rules.reduce((best, [rgx, fn])=>{
+			let match = rgx.exec(remaining);
+			if(!match) return best;
+			if(!best || match.index < best.index){ match.func = fn; return match; }
+			return best;
+		}, false);
+		if(!match) break;
+		const [matchedText, ...groups] = match;
+		result.push(match.func(groups, matchedText, remaining.substring(0,match.index)));
+		remaining = remaining.substring(match.index + matchedText.length);
 	}
-
-
-	const context = (input, exitRule=/ /)=>{
-		let result = [];
-		const run = (text, ctx, idx=0)=>{
-			let bestMatch = getBestMatch({'__exit':{regex:exitRule}, ...Rules}, text);
-
-			if(!bestMatch){
-				if(defaultRule) result.push(defaultRule(text));
-				return [result, text];
-			}
-			if(bestMatch.id == '__exit'){
-				if(defaultRule) result.push(defaultRule(text.substring(0,bestMatch.start)));
-				return [result, text.substring(bestMatch.end), bestMatch.groups];
-			}
-
-			const bestRule = Rules[bestMatch.id];
-
-			if(bestRule.nest){
-				const [subResult, rest, groups] = context(text.substring(bestMatch.end), bestRule.nest);
-				result.push(bestRule.render(bestMatch, bestMatch.groups.concat(groups||[]), subResult));
-				return run(rest)
-			}
-
-			result.push(bestRule.render(bestMatch, bestMatch.groups));
-			return run(text.substring(bestMatch.end));
-
-		}
-		return run(input)
-	};
-	return context(input)[0]
-}
-module.exports = engine
+	return result;
+};
