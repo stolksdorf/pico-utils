5a702177961ec85e78b283906cfaeb7a701cf248
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2021-11-29 14:34:36 -0500

    Update other.js

--- other.js
+++ other.js
@@ -89,7 +89,7 @@ const cookies = document.cookie.split(';').reduce((res, c) => {
 }, {});
 
 const _internalPaths = Object.keys(process.binding('natives')).concat(['bootstrap_node', 'node']).map((name) => `${name}.js`);
-const getStack = (idx=false)=>{
+const getStackOld = (idx=false)=>{
 	const stack = (new Error()).stack.split('\n').map((raw)=>{
 		const [_, name, file, line, col] =
 			/    at (.*?) \((.*?):(\d*):(\d*)\)/.exec(raw) || /    at ()(.*?):(\d*):(\d*)/.exec(raw) || [];
@@ -98,3 +98,17 @@ const getStack = (idx=false)=>{
 	if(idx !== false) return stack[idx];
 	return stack;
 };
+
+
+// You can call this with no args, an error object, or a error stack as a string
+const getStack = (target = (new Error()).stack)=>{
+	let [msg, ...stack] = (target.stack || target).split('\n');
+	stack = stack .map((raw)=>{
+		const [_, scope, file, line, col] =
+			/    at (.*?) \((.*?):(\d*):(\d*)\)/.exec(raw) || /    at ()(.*?):(\d*):(\d*)/.exec(raw) || [];
+		return { scope, file, line : Number(line), col  : Number(col), raw };
+	});
+	return {msg, stack};
+};
+
+
