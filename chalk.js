a2ee974a75efe7ab38d02cb98bba0d7f05567ace
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2021-12-09 11:14:15 -0500

    Update chalk.js

--- chalk.js
+++ chalk.js
@@ -1,10 +1,11 @@
 const chalk = Object.entries({
 	bright: 1,  dim:   2,  red:  31,
 	green:  32, yellow:33, blue: 34,
-	magenta:35, cyan:  36, white:37
+	magenta:35, cyan:  36, white:37,
+	black:  30, grey:  90, gray: 90
 }).reduce((acc, [name, id])=>{return {...acc, [name]:(txt)=>`\x1b[${id}m${txt}\x1b[0m`}});
 
 //One Liner
-const chalk = Object.entries({bright:1,dim:2,red:31,green:32,yellow:33,blue:34,magenta:35,cyan:36,white:37}).reduce((acc, [name, id])=>{return {...acc, [name]:(txt)=>`\x1b[${id}m${txt}\x1b[0m`}});
+const chalk = Object.entries({bright:1,dim:2,red:31,green:32,yellow:33,blue:34,magenta:35,cyan:36,white:37,black:30,grey:90}).reduce((acc, [name, id])=>{return {...acc, [name]:(txt)=>`\x1b[${id}m${txt}\x1b[0m`}});
 
 module.exports = chalk;
