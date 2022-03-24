d8ad6af8ed464ec33b0039e45acfd8b1d81e63ca
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2022-03-24 13:41:47 -0400

    Update async.js

--- async.js
+++ async.js
@@ -3,6 +3,12 @@ const loop = async (fn)=>setTimeout(()=>loop(fn), await fn() || 1000);
 const sequence = async (obj, fn)=>Object.keys(obj).reduce((a,key)=>a.then((r)=>fn(obj[key], key, r)), Promise.resolve());
 const debounce = (fn, t=16)=>function(...args){clearTimeout(this.clk);this.clk=setTimeout(()=>fn(...args),t);};
 
+
+const sequence = async (obj, fn)=>{
+	let res = Array.isArray(obj)?[]:{};
+	return Object.keys(obj).reduce((p,k)=>p.then(()=>fn(obj[k], k)).then(r=>res[k]=r), Promise.resolve()).then(()=>res);
+};
+
 const debounce = (fn, t=16)=>{ let timeout; return (...args)=>{clearTimeout(timeout); timeout=setTimeout(()=>fn(...args), t);}};
 const throttle = (fn, t=16)=>{ let timeout; return (...args)=>{ if(timeout){return;}  timeout=setTimeout(()=>{timeout=false;fn(...args)}, t);}}
 
